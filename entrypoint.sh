f5096243718a8aaf4bc98325daba0de228bb55f9
PgrammerStud <PgrammerStud>
2026-05-21 17:48:33 +0800

    fix: change code

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,5 @@
-$content = @'
 #!/bin/bash
 set -e
-
-echo "Clearing and warming up cache..."
-php bin/console cache:clear --env=prod --no-debug
-php bin/console cache:warmup --env=prod --no-debug
 
 echo "Starting PHP-FPM..."
 php-fpm -F &
@@ -17,7 +12,3 @@ echo "Starting Nginx..."
 nginx -g "daemon off;"
 
 wait $PHP_PID
-'@
-
-$content = $content -replace "`r`n", "`n"
-[System.IO.File]::WriteAllText("$PWD\entrypoint.sh", $content)
