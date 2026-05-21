28a2dfe699b1c21f1aa8d9db77b80cd79d51919a
PgrammerStud <PgrammerStud>
2026-05-21 19:04:59 +0800

    fix: asset map compile

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,9 @@ echo "Running Symfony cache clear & warmup..."
 php /app/bin/console cache:clear --env=prod --no-debug
 php /app/bin/console cache:warmup --env=prod --no-debug
 
+echo "Dumping assets..."
+php /app/bin/console asset-map:compile --no-interaction
+
 echo "Fixing permissions after cache warmup..."
 chown -R www-data:www-data /app/var
 chmod -R 775 /app/var
